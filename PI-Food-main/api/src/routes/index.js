87793229fefd6c277a752e1d74e6ab1a71513d92
mauroociappinaph <ciappinamauroo@gmail.com>
2023-04-12 20:06:51 -0300

    Back

--- PI-Food-main/api/src/routes/index.js
+++ PI-Food-main/api/src/routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 const recipe = require('./recipe.js');
 const recipes = require('./recipes.js');
 const diet = require('./diets.js');
